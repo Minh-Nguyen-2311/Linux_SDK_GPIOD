27294ed38be4e7acba21a842a1c831d966913849
Minh-Nguyen-2311 <115912432+Minh-Nguyen-2311@users.noreply.github.com>
2025-10-23 14:18:18 +0700

    Delete servo_sdk directory

--- servo_sdk/include/servo_driver.hpp
+++
@@ -1,8 +0,0 @@
-#include "stdio.h"
-#include "math.h"
-#include "gpiod.h"
-
-int servo_driver_init(const char *chipname, unsigned int line_num);
-void servo_driver_set_angle(float angle);
-void servo_driver_set_direction(float speed);
-void servo_driver_stop(void);
